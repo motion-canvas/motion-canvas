884673a65c27dccbeda77522f5a1d9821637f1a9
aarthificial <aarthificial@gmail.com>
2022-04-09 01:09:50 +0200

    refactor: loaders performance improvement

--- tools/utils/load-image.js
+++ tools/utils/load-image.js
@@ -7,23 +7,23 @@ const context = nodeCanvas.createCanvas(SIZE, SIZE).getContext('2d');
 const image = new nodeCanvas.Image();
 
 module.exports = async function (fileName) {
-    const data = await fs.readFile(fileName, 'base64');
-    const dimensions = sizeOf(fileName);
+  const buffer = await fs.readFile(fileName);
+  const dimensions = sizeOf(buffer);
 
-    image.src = `data:image/png;base64,${data}`;
-    context.clearRect(0, 0, SIZE, SIZE);
-    context.drawImage(image, 0, 0, dimensions.width, dimensions.height);
-    const imageData = context.getImageData(
-        0,
-        0,
-        dimensions.width,
-        dimensions.height,
-    );
+  image.src = `data:image/png;base64,${buffer.toString('base64')}`;
+  context.clearRect(0, 0, SIZE, SIZE);
+  context.drawImage(image, 0, 0, dimensions.width, dimensions.height);
+  const imageData = context.getImageData(
+    0,
+    0,
+    dimensions.width,
+    dimensions.height,
+  );
 
-    return {
-        fileName,
-        data: Array.from(imageData.data),
-        width: dimensions.width,
-        height: dimensions.height,
-    };
-}
+  return {
+    fileName,
+    data: Array.from(imageData.data),
+    width: dimensions.width,
+    height: dimensions.height,
+  };
+};
