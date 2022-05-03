60ccda723361751f28bc1144de314388551c95a2
aarthificial <aarthificial@gmail.com>
2022-05-03 23:16:50 +0200

    feat: rendering

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
 import styles from './Sidebar.module.scss';
 
+import type {PlayerRenderEvent} from '@motion-canvas/core/player/Player';
 import {IconType} from '../controls';
 import {Tabs} from '../tabs/Tabs';
-import {usePlayer} from '../../hooks';
-import {useCallback, useEffect, useState} from 'preact/hooks';
+import {usePlayer, usePlayerState} from '../../hooks';
+import {useCallback, useEffect, useRef, useState} from 'preact/hooks';
 import {Thread} from '@motion-canvas/core/threading';
 import {GeneratorHelper} from '@motion-canvas/core/helpers';
 
@@ -20,13 +21,55 @@ export function Sidebar({setOpen}: SidebarProps) {
       }}
       {{
         icon: IconType.videoSettings,
-        pane: <div className={styles.pane}>Rendering</div>,
+        pane: <Rendering />,
       }}
       {{
         icon: IconType.schedule,
         pane: <Threads />,
       }}
     </Tabs>
+  );
+}
+
+function Rendering() {
+  const player = usePlayer();
+  const state = usePlayerState();
+  const directoryHandle = useRef<FileSystemDirectoryHandle>();
+
+  const handleRender = useCallback(async ({frame, blob}: PlayerRenderEvent) => {
+    const name = frame.toString().padStart(6, '0');
+    const size = blob.size / 1024;
+
+    try {
+      directoryHandle.current ??= await window.showDirectoryPicker();
+      const file = await directoryHandle.current.getFileHandle(
+        `frame-${name}.png`,
+        {
+          create: true,
+        },
+      );
+      const stream = await file.createWritable();
+      await stream.write(blob);
+      await stream.close();
+      console.log(`Frame: ${name}, Size: ${Math.round(size)} kB`);
+    } catch (e) {
+      console.error(e);
+      player.toggleRendering(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    player.RenderChanged.subscribe(handleRender);
+    return () => player.RenderChanged.unsubscribe(handleRender);
+  }, [handleRender]);
+
+  return (
+    <div className={styles.pane}>
+      <div className={styles.header}>Rendering</div>
+      <button onClick={() => player.toggleRendering()}>
+        {state.render ? 'RENDERING...' : 'RENDER'}
+      </button>
+    </div>
   );
 }
 
