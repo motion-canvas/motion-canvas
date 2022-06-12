cc86a4a6d5b44e75ed02a1bdf90b588450a663b2
Jacob <64662184+aarthificial@users.noreply.github.com>
2022-06-12 22:27:18 +0200

    fix: restrict the corner radius of a rectangle (#9)
    The maximum radius can't be bigger than half the size of the rectangle.
    
    Fixes #8

--- src/helpers/CanvasHelper.ts
+++ src/helpers/CanvasHelper.ts
@@ -25,8 +25,13 @@ export const CanvasHelper = {
     height: number,
     radius: PossibleSpacing,
   ): T {
-    //FIXME Handle too small radii
     const spacing = new Spacing(radius);
+    const maxRadius = Math.min(height / 2, width / 2);
+    spacing.left = Math.min(spacing.left, maxRadius);
+    spacing.right = Math.min(spacing.right, maxRadius);
+    spacing.top = Math.min(spacing.top, maxRadius);
+    spacing.bottom = Math.min(spacing.bottom, maxRadius);
+
     ctx.moveTo(x + spacing.left, y);
     ctx.arcTo(x + width, y, x + width, y + height, spacing.top);
     ctx.arcTo(x + width, y + height, x, y + height, spacing.right);
