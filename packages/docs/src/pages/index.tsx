a29fcfd4bab9bad99376d5544dada570248ad0ef
aarthificial <aarthificial@gmail.com>
2022-08-04 16:26:34 +0200

    feat(docs): add logo

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -32,10 +32,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
