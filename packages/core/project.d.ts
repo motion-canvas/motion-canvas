325c2442814ca19407fe0060a819aded4456f90e
Jacob <64662184+aarthificial@users.noreply.github.com>
2022-08-15 02:42:00 +0200

    fix(core): add missing type references (#41)

--- packages/core/project.d.ts
+++ packages/core/project.d.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-namespace */
 /// <reference types="vite/client" />
+/// <reference types="@motion-canvas/core/lib/patches/Factory" />
+/// <reference types="@motion-canvas/core/lib/patches/Node" />
+/// <reference types="@motion-canvas/core/lib/patches/Shape" />
+/// <reference types="@motion-canvas/core/lib/patches/Container" />
 
 declare module '*?img' {
   const value: Promise<HTMLImageElement>;
