842079a6547af4032719c85837df3db7c1c6d30a
aarthificial <aarthificial@gmail.com>
2022-11-17 16:45:20 +0100

    feat: implement absolute scale setter

--- packages/core/src/utils/range.ts
+++ packages/core/src/utils/range.ts
@@ -1,13 +1,44 @@
 /**
- * Create an array of given length containing its own indices.
+ * Create an array containing a range of numbers.
  *
  * @example
  * ```ts
  * const array = range(3); // [0, 1, 2]
  * ```
  *
- * @param length
+ * @param length - The length of the array.
  */
-export function range(length: number): number[] {
-  return [...Array(length).keys()];
+export function range(length: number): number[];
+/**
+ * Create an array containing a range of numbers.
+ *
+ * @example
+ * ```ts
+ * const array = range(3, 7); // [3, 4, 5, 6]
+ * ```
+ *
+ * @param from - The start of the range.
+ * @param to - The end of the range. `to` itself is not included in the result.
+ */
+export function range(from: number, to: number): number[];
+export function range(first: number, second?: number): number[] {
+  let from = 0;
+  let to = first;
+  if (second !== undefined) {
+    from = first;
+    to = second;
+  }
+
+  const array = [];
+  if (from > to) {
+    for (let i = from; i > to; i--) {
+      array.push(i);
+    }
+  } else {
+    for (let i = from; i < to; i++) {
+      array.push(i);
+    }
+  }
+
+  return array;
 }
