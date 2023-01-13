da0e104451af72eedb3eedd998f60b305fffdb0e
Jacob <64662184+aarthificial@users.noreply.github.com>
2023-01-13 07:35:25 +0100

    feat: add deprecation support (#130)

--- packages/examples/src/scenes/tweening-linear.tsx
+++ packages/examples/src/scenes/tweening-linear.tsx
@@ -1,10 +1,10 @@
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
 import {Circle} from '@motion-canvas/2d/lib/components';
-import {useRef} from '@motion-canvas/core/lib/utils';
+import {createRef} from '@motion-canvas/core/lib/utils';
 import {tween, map} from '@motion-canvas/core/lib/tweening';
 
 export default makeScene2D(function* (view) {
-  const circle = useRef<Circle>();
+  const circle = createRef<Circle>();
 
   view.add(
     <Circle
