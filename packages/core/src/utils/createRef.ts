47fb410ae5d23be04cc6ced2e467523a93e3d62d
aarthificial <aarthificial@gmail.com>
2023-01-17 01:33:25 +0100

    feat: unify references and signals

--- packages/core/src/utils/createRef.ts
+++ packages/core/src/utils/createRef.ts
@@ -1,23 +1,65 @@
-export type Reference<TValue> = TValue extends (config: {
-  ref?: infer TReference;
-}) => void
-  ? TReference
-  : {value: TValue};
+import {deprecate} from './deprecate';
+
+export interface ReferenceReceiver<T> {
+  (reference: T): void;
+}
+
+export interface Reference<T> extends ReferenceReceiver<T> {
+  (): T;
+
+  /**
+   * @deprecated Invoke the reference instead.
+   */
+  value: T;
+}
 
 export function createRef<T>(): Reference<T> {
-  return {} as Reference<T>;
+  let value: T;
+  const ref = (newValue?: T) => {
+    if (newValue !== undefined) {
+      value = newValue;
+    } else {
+      return value;
+    }
+  };
+
+  Object.defineProperty(ref, 'value', {
+    get: deprecate(
+      () => value,
+      'get Reference.value has been deprecated.',
+      `To retrieve the referenced object, invoke the reference like a function:
+      <pre>ref.value; // wrong\nref(); // correct</pre>`,
+    ),
+    set: deprecate(
+      newValue => {
+        value = newValue;
+      },
+      'set Reference.value has been deprecated.',
+      `To set the referenced object, pass it to the reference like you would to a function:
+      <pre>ref.value = object; // wrong\nref(object); // correct</pre>`,
+    ),
+  });
+
+  return ref as Reference<T>;
 }
 
 export function makeRef<TObject, TKey extends keyof TObject>(
   object: TObject,
   key: TKey,
-): Reference<TObject[TKey]> {
-  return {
-    get value(): TObject[TKey] {
-      return object[key];
-    },
-    set value(value: TObject[TKey]) {
-      object[key] = value;
-    },
-  } as Reference<TObject[TKey]>;
+): ReferenceReceiver<TObject[TKey]> {
+  return newValue => {
+    object[key] = newValue;
+  };
+}
+
+export type RefsProperty<TValue> = TValue extends (config: {
+  refs?: infer TReference;
+}) => void
+  ? TReference
+  : never;
+
+export function makeRefs<
+  T extends (config: {refs?: any}) => void,
+>(): RefsProperty<T> {
+  return {} as RefsProperty<T>;
 }
