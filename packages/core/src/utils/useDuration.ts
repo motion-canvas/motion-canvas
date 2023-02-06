8dea1d8212d352cb950d92dc4ea6014a11f36091
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-02-06 20:44:48 +0100

    feat: add `useDuration` helper
    Adding a `useDuration` helper function to easily
    create a `TimeEvent` and use it to control an
    animation.
    
    Issue: #171

---
+++ packages/core/src/utils/useDuration.ts
@@ -0,0 +1,31 @@
+import {useScene} from './useScene';
+
+/**
+ * Register a time event and get its duration in seconds.
+ *
+ * @remarks
+ * This can be used to better specify when an animation should start
+ * as well as how long this animation should take
+ *
+ * @example
+ * ```ts
+ * export default makeScene2D(function* (view) {
+ *   const circle = createRef<Circle>();
+ *
+ *   view.add(
+ *     <Circle ref={circle} width={320} height={320} fill={'lightseagreen'} />,
+ *   );
+ *
+ *   yield* circle().scale(2, useDuration('circleGrow'));
+ * });
+ * ```
+ *
+ * @param name - The name of the event.
+ *
+ * @returns The duration of the event in seconds.
+ */
+export function useDuration(name: string): number {
+  const scene = useScene();
+  scene.timeEvents.register(name);
+  return scene.timeEvents.get(name).offset;
+}
