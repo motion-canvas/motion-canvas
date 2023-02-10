6a90785ea87954b5ede1baf726bdb11bc39723f1
SecondThundeR <awayfromgalaxy@gmail.com>
2023-02-10 11:32:13 +0300

    fix(ui): set select appearance to none

--- packages/ui/src/components/playback/PlaybackControls.tsx
+++ packages/ui/src/components/playback/PlaybackControls.tsx
@@ -62,6 +62,7 @@ export function PlaybackControls() {
   return (
     <div className={clsx(styles.controls, state.render && styles.disabled)}>
       <Select
+        className={clsx(styles.select)}
         title="Playback speed"
         options={[
           {value: 0.25, text: 'x0.25'},
