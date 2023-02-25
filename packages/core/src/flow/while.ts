1c0e2eb55fc9efe32d7e965fb8c71887c4340e18
Squiggle-Development <luke.saynor@gmail.com>
2023-02-25 17:37:34 +0000

    feat(core): add `loopUntil()` function

---
+++ packages/core/src/flow/while.ts
@@ -0,0 +1,38 @@
+import {useScene} from '../utils';
+import {decorate, threadable} from '../decorators';
+import {ThreadGenerator} from '../threading';
+import {LoopCallback} from './loop';
+
+decorate(loopUntil, threadable());
+/**
+ * Run the given generator until an event occurs.
+ *
+ * @example
+ * ```ts
+ * yield* loopUntil(
+ *  'vibrate', () =\>
+ *  circle().position.x(-10,0.1).to(10,0.1),
+ * );
+ * ```
+ *
+ * @param event - The event.
+ * @param tasks - A list of tasks to run.
+ *
+ */
+export function* loopUntil(
+  event: string,
+  factory: LoopCallback,
+): ThreadGenerator {
+  const scene = useScene();
+  const frames = scene.timeEvents.register(event);
+  let iterator = 0;
+  while (scene.project.frame < frames) {
+    const generator = factory(iterator);
+    if (generator) {
+      yield* generator;
+    } else {
+      yield;
+    }
+    iterator += 1;
+  }
+}
