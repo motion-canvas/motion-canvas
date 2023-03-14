87a477c60c946705ecc60a7f0be495893be1de36
Ashish Baravaliya <ashishbaravaliya@Ashishs-Air.cgocable.net>
2023-03-14 18:45:44 -0400

    fix(ui): added prerelease support to compareversions func

--- packages/ui/src/utils/compareVersions.tsx
+++ packages/ui/src/utils/compareVersions.tsx
@@ -4,15 +4,37 @@ export function compareVersions(v1: string, v2: string): number {
   const len = Math.max(v1Parts.length, v2Parts.length);
 
   for (let i = 0; i < len; i++) {
-    const v1Part = parseInt(v1Parts[i] || '0', 10);
-    const v2Part = parseInt(v2Parts[i] || '0', 10);
-    if (v1Part > v2Part) {
-      return 1;
-    }
-    if (v1Part < v2Part) {
-      return -1;
+    const v1Part = getNumericPart(v1Parts[i]);
+    const v2Part = getNumericPart(v2Parts[i]);
+
+    if (v1Part === v2Part) {
+      const v1Pre = getPreReleasePart(v1Parts[i]);
+      const v2Pre = getPreReleasePart(v2Parts[i]);
+
+      if (v1Pre && v2Pre && v1Pre !== v2Pre) {
+        return v1Pre.localeCompare(v2Pre);
+      } else if (v1Pre && !v2Pre) {
+        return -1;
+      } else if (!v1Pre && v2Pre) {
+        return 1;
+      }
+    } else {
+      return v1Part > v2Part ? 1 : -1;
     }
   }
 
   return 0;
+}
+
+function getNumericPart(part: string): string | number {
+  const numPart = parseInt(part, 10);
+  return isNaN(numPart) ? part : numPart;
+}
+
+function getPreReleasePart(part: string): string {
+  if (!part) {
+    return null;
+  }
+  const preReleaseMatch = part.match(/-([0-9A-Za-z-.]+)/);
+  return preReleaseMatch ? preReleaseMatch[1] : null;
 }
