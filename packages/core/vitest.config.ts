65fec7825fda33812b13f57bfeb1d82193a5d190
Jacob <64662184+aarthificial@users.noreply.github.com>
2023-03-14 19:33:00 +0100

    feat(core): tree shaking (#523)
    This PR makes tree shaking work correctly.
    The `CodeBlock` and `Latex` nodes are now correctly excluded when not used in the project.

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -4,7 +4,6 @@ import markdownLiterals from '@motion-canvas/internal/vite/markdown-literals';
 export default defineConfig({
   plugins: [markdownLiterals()],
   test: {
-    setupFiles: ['./vitest.setup.ts'],
     environment: 'jsdom',
   },
 });
