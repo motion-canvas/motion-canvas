572dfa0499ee419755e4a789026bc5f029bb2702
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-03-29 06:29:58 +0700

    docs(2d): update latex documentation

--- packages/examples/src/scenes/tex.tsx
+++ packages/examples/src/scenes/tex.tsx
@@ -1,16 +1,11 @@
-import {Latex, Rect} from '@motion-canvas/2d/lib/components';
+import {Latex} from '@motion-canvas/2d/lib/components';
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
 import {waitFor} from '@motion-canvas/core/lib/flow';
 import {createRef} from '@motion-canvas/core/lib/utils';
 
 export default makeScene2D(function* (view) {
-  console.log('Animation started');
   const tex = createRef<Latex>();
-  view.add(
-    <Rect layout>
-      <Latex ref={tex} tex={['y', '=', '{{a}}{{x^2}}']} fill="white" />
-    </Rect>,
-  );
+  view.add(<Latex ref={tex} tex={['y', '=', '{{a}}{{x^2}}']} fill="white" />);
 
   yield* waitFor(0.5);
   yield* tex().tex(['y', '=', '{{a}}{{x^2}} + {{bx}}'], 1);
@@ -21,4 +16,5 @@ export default makeScene2D(function* (view) {
   yield* waitFor(0.5);
   yield* tex().tex(['y', '=', '{{x^2}}'], 1);
   yield* waitFor(0.5);
+  yield* tex().tex(['y', '=', '{{a}}{{x^2}}'], 1);
 });
