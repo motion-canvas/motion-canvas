f93c8ec7590d238cc7e34be5d859429ca176b044
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-03-29 05:14:10 +0700

    fix(2d): simplify tex example

--- packages/examples/src/scenes/tex.tsx
+++ packages/examples/src/scenes/tex.tsx
@@ -4,120 +4,21 @@ import {waitFor} from '@motion-canvas/core/lib/flow';
 import {createRef} from '@motion-canvas/core/lib/utils';
 
 export default makeScene2D(function* (view) {
+  console.log('Animation started');
   const tex = createRef<Latex>();
   view.add(
     <Rect layout>
-      <Latex
-        ref={tex}
-        tex={['x', '=', '{{\\sin \\left(}} \\pi {{\\over}} 2 {{\\right)}}']}
-        fill="white"
-      />
+      <Latex ref={tex} tex={['y', '=', '{{a}}{{x^2}}']} fill="white" />
     </Rect>,
   );
 
-  yield* waitFor(2);
-  yield* tex().opacity(0, 1);
-  yield* tex().opacity(1, 1);
-  yield* waitFor(2);
-  yield* tex().tex(
-    ['x', '=', '{{\\sin \\left(}} \\pi {{\\over}} 2 {{\\right)}}', '+', '3'],
-    1,
-  );
-  yield* tex().tex(
-    [
-      'x',
-      '=',
-      '1',
-      '+',
-      '{{\\sin \\left(}} \\pi {{\\over}} 2 {{\\right)}}',
-      '+',
-      '3',
-    ],
-    1.2,
-  );
-  yield* tex().tex(
-    [
-      'x',
-      '=',
-      '5',
-      '+',
-      '9',
-      '+',
-      '{{\\sin \\left(}} \\pi {{\\over}} 2 {{\\right)}}',
-      '+',
-      '7',
-      '+',
-      '8',
-    ],
-    1.2,
-  );
-  yield* tex().tex(
-    [
-      'x',
-      '=',
-      '5',
-      '+',
-      '2',
-      '+',
-      '9',
-      '+',
-      '{{\\sin \\left(}} \\pi {{\\over}} 2 {{\\right)}}',
-      '+',
-      '7',
-      '+',
-      '1',
-      '+',
-      '8',
-    ],
-    1.2,
-  );
-  yield* tex().tex(
-    [
-      'x',
-      '=',
-      '7',
-      '+',
-      '1',
-      '+',
-      '8',
-      '+',
-      '{{\\sin \\left(}} \\pi {{\\over}} 2 {{\\right)}}',
-      '+',
-      '5',
-      '+',
-      '2',
-      '+',
-      '9',
-    ],
-    1.2,
-  );
-  yield* tex().tex(
-    [
-      'x',
-      '=',
-      '3',
-      '+',
-      '{{\\sin \\left(}} \\pi + 3 {{\\over}} 2 {{\\right)}}',
-      '+',
-      '1',
-    ],
-    1.2,
-  );
-  yield* tex().tex(
-    [
-      'x',
-      '=',
-      '{{\\sin \\left(}} \\pi + 3 {{\\over}} 2 {{\\right)}}',
-      '+',
-      '3',
-      '+',
-      '1',
-    ],
-    1.2,
-  );
-  yield* tex().tex(
-    ['x', '=', '{{\\sin \\left(}} \\pi {{\\over}} 2 {{\\right)}}'],
-    1.2,
-  );
-  yield* waitFor(2);
+  yield* waitFor(0.5);
+  yield* tex().tex(['y', '=', '{{a}}{{x^2}} + {{bx}}'], 1);
+  yield* waitFor(0.5);
+  yield* tex().tex(['y', '=', '{{c}} + {{a}}{{x^2}} + {{bx}}'], 1);
+  yield* waitFor(0.5);
+  yield* tex().tex(['y', '=', '{{a}}{{x^2}} + {{bx}} + {{c}}'], 1);
+  yield* waitFor(0.5);
+  yield* tex().tex(['y', '=', '{{x^2}}'], 1);
+  yield* waitFor(0.5);
 });
