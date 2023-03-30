242f50a0149815e63233aaafef0cefa77fbb8dd4
Waldemar Lehner <mail@waldemarlehner.de>
2023-03-30 21:10:08 +0200

    fixup! feat(docs): add Canvas Operations

--- packages/examples/src/components/AnimatedMotionCanvasIcon.tsx
+++ packages/examples/src/components/AnimatedMotionCanvasIcon.tsx
@@ -41,93 +41,89 @@ export class AnimatedMotionCanvasIcon extends Node {
     const GREEN = '#99C47A';
 
     this.add(
-      <>
-        <Layout layout={false}>
-          <Node rotation={-45} position={44} scale={0.8}>
-            <Node cache>
-              <Node cache rotation={() => this.timePassed() * 360}>
-                {range(5).map(i => (
+      <Node rotation={-45} position={44} scale={0.8}>
+        <Node cache>
+          <Node cache rotation={() => this.timePassed() * 360}>
+            {range(5).map(i => (
+              <Rect
+                width={100}
+                radius={50}
+                height={150}
+                fill={'#36393F'}
+                offsetY={1}
+                rotation={(360 / 5) * i}
+              />
+            ))}
+          </Node>
+          <Node compositeOperation={'source-out'}>
+            {/* Trails */}
+            <Node cache y={-270}>
+              <Trail y={() => (-150 * 4 * this.timePassed()) % 150}>
+                {range(3).map(() => (
+                  <Rect width={40} radius={20} height={120} fill={YELLOW} />
+                ))}
+              </Trail>
+              <Rect
+                width={40}
+                radius={20}
+                height={270}
+                fill={'white'}
+                offsetY={-1}
+                compositeOperation={'destination-in'}
+              />
+            </Node>
+            <Node cache x={-70} y={-200}>
+              <Trail y={() => (-150 * 2 * this.timePassed()) % 150}>
+                {range(3).map(() => (
+                  <Rect width={40} height={120} radius={20} fill={RED} />
+                ))}
+              </Trail>
+              <Rect
+                width={40}
+                radius={20}
+                height={180}
+                fill={'white'}
+                offsetY={-1}
+                compositeOperation={'destination-in'}
+              />
+            </Node>
+            <Node cache x={70} y={-300}>
+              <Trail y={() => (-130 * 2 * this.timePassed()) % 130}>
+                {range(4).map(() => (
                   <Rect
-                    width={100}
-                    radius={50}
-                    height={150}
-                    fill={'#36393F'}
+                    width={40}
+                    radius={20}
+                    height={100}
+                    fill={GREEN}
                     offsetY={1}
-                    rotation={(360 / 5) * i}
                   />
                 ))}
-              </Node>
-              <Node compositeOperation={'source-out'}>
-                {/* Trails */}
-                <Node cache y={-270}>
-                  <Trail y={() => (-150 * 4 * this.timePassed()) % 150}>
-                    {range(3).map(() => (
-                      <Rect width={40} radius={20} height={120} fill={YELLOW} />
-                    ))}
-                  </Trail>
-                  <Rect
-                    width={40}
-                    radius={20}
-                    height={270}
-                    fill={'white'}
-                    offsetY={-1}
-                    compositeOperation={'destination-in'}
-                  />
-                </Node>
-                <Node cache x={-70} y={-200}>
-                  <Trail y={() => (-150 * 2 * this.timePassed()) % 150}>
-                    {range(3).map(() => (
-                      <Rect width={40} height={120} radius={20} fill={RED} />
-                    ))}
-                  </Trail>
-                  <Rect
-                    width={40}
-                    radius={20}
-                    height={180}
-                    fill={'white'}
-                    offsetY={-1}
-                    compositeOperation={'destination-in'}
-                  />
-                </Node>
-                <Node cache x={70} y={-300}>
-                  <Trail y={() => (-130 * 2 * this.timePassed()) % 130}>
-                    {range(4).map(() => (
-                      <Rect
-                        width={40}
-                        radius={20}
-                        height={100}
-                        fill={GREEN}
-                        offsetY={1}
-                      />
-                    ))}
-                  </Trail>
-                  <Rect
-                    width={40}
-                    radius={20}
-                    height={220}
-                    fill={'white'}
-                    offsetY={-1}
-                    y={60}
-                    compositeOperation={'destination-in'}
-                  />
-                </Node>
-              </Node>
-            </Node>
-            <Node rotation={() => this.timePassed() * 360}>
-              {range(5).map(i => (
-                <Rect
-                  width={40}
-                  radius={20}
-                  height={120}
-                  fill={'white'}
-                  offsetY={1}
-                  rotation={(360 / 5) * i}
-                />
-              ))}
+              </Trail>
+              <Rect
+                width={40}
+                radius={20}
+                height={220}
+                fill={'white'}
+                offsetY={-1}
+                y={60}
+                compositeOperation={'destination-in'}
+              />
             </Node>
           </Node>
-        </Layout>
-      </>,
+        </Node>
+        <Node rotation={() => this.timePassed() * 360}>
+          {range(5).map(i => (
+            <Rect
+              width={40}
+              radius={20}
+              height={120}
+              fill={'white'}
+              offsetY={1}
+              rotation={(360 / 5) * i}
+            />
+          ))}
+        </Node>
+      </Node>,
     );
   }
 }
