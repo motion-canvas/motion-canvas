104328fca11a2f2fda36006e1f10c8dd73260252
aarthificial <aarthificial@gmail.com>
2023-04-06 20:13:36 +0200

    docs: improve 3.5.0 release

--- packages/docs/blog/2023-04-06-examples.tsx
+++ packages/docs/blog/2023-04-06-examples.tsx
@@ -1,3 +1,4 @@
+// snippet Cubic Bezier
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
 import {CubicBezier} from '@motion-canvas/2d/lib/components';
 import {createRef} from '@motion-canvas/core/lib/utils';
@@ -14,6 +15,29 @@ export default makeScene2D(function* (view) {
       p1={[50, -200]}
       p2={[-50, 200]}
       p3={[200, 0]}
+    />,
+  );
+
+  yield* bezier().start(1, 1);
+  yield* bezier().start(0).end(0).end(1, 1);
+});
+
+// snippet Quadratic Bezier
+import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
+import {QuadBezier} from '@motion-canvas/2d/lib/components';
+import {createRef} from '@motion-canvas/core/lib/utils';
+
+export default makeScene2D(function* (view) {
+  const bezier = createRef<QuadBezier>();
+
+  view.add(
+    <QuadBezier
+      ref={bezier}
+      lineWidth={6}
+      stroke={'lightseagreen'}
+      p0={[-200, 70]}
+      p1={[0, -200]}
+      p2={[200, 70]}
     />,
   );
 
