5e82dfe52142eb7dc617eab5c511d3ed7b96dc23
Eugene Pirogov <iamexile@gmail.com>
2023-04-29 19:59:41 +0200

    docs: convert "transitions-second" in "transitions.mdx" to AnimationEditor

--- packages/examples/src/transitions.ts
+++ packages/examples/src/transitions.ts
@@ -1,8 +1,7 @@
 import {makeProject} from '@motion-canvas/core';
 
 import first from './scenes/transitions-first?scene';
-import second from './scenes/transitions-second?scene';
 
 export default makeProject({
-  scenes: [first, second],
+  scenes: [first],
 });
