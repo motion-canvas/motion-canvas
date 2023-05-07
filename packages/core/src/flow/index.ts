18ed157dbc35fe85abfc758cadd3a74fe7b0869c
aarthificial <aarthificial@gmail.com>
2023-05-07 14:27:03 +0200

    feat(core): add `loopFor` function

--- packages/core/src/flow/index.ts
+++ packages/core/src/flow/index.ts
@@ -9,6 +9,7 @@ export * from './chain';
 export * from './delay';
 export * from './every';
 export * from './loop';
+export * from './loopFor';
 export * from './loopUntil';
 export * from './noop';
 export * from './run';
