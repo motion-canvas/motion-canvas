5e18892e18a82d92e1bcd49ef2e8ea489dcab5ac
SleeklyCoding <129587457+SleeklyCoding@users.noreply.github.com>
2023-08-02 08:54:05 +0200

    fix(vite-plugin): fix 'OUTPUT DIRECTORY' button on WSL

--- packages/vite-plugin/src/openInExplorer.ts
+++ packages/vite-plugin/src/openInExplorer.ts
@@ -1,4 +1,4 @@
-import {spawn} from 'child_process';
+import {execSync, spawn} from 'child_process';
 import {platform} from 'os';
 import {existsSync, mkdirSync} from 'fs';
 
@@ -15,16 +15,33 @@ export function openInExplorer(file: string) {
       explorer = 'explorer';
       break;
     case 'linux':
-      explorer = 'xdg-open';
+      if (isRunningOnWSL()) {
+        explorer = 'bash';
+        file = `cd ${file} && explorer.exe .`;
+      } else {
+        explorer = 'xdg-open';
+      }
       break;
     case 'darwin':
       explorer = 'open';
       break;
   }
 
-  if (explorer) {
+  if (explorer === 'bash') {
+    spawn(explorer, ['-c', file], {detached: true}).unref();
+  } else if (explorer) {
     spawn(explorer, [file], {detached: true}).unref();
   } else {
     console.warn(`Unsupported OS: ${os}`);
+  }
+}
+
+function isRunningOnWSL(): boolean {
+  try {
+    const uname = execSync('uname -a').toString().toLowerCase();
+    return uname.includes('microsoft');
+  } catch (error) {
+    console.error(`exec error: ${error}`);
+    return false;
   }
 }
