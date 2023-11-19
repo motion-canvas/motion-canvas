ae0f029cf9499d56375e7c590db6f6d7088e38e3
aarthificial <aarthificial@gmail.com>
2023-11-19 15:14:04 +0100

    feat: improve image error handling

--- packages/core/src/utils/DetailedError.ts
+++ packages/core/src/utils/DetailedError.ts
@@ -1,5 +1,40 @@
+import type {LogPayload} from '../app';
+
+type DetailedErrorProps = Pick<
+  LogPayload,
+  'message' | 'remarks' | 'object' | 'durationMs' | 'inspect'
+>;
+
 export class DetailedError extends Error {
-  public constructor(message: string, public readonly remarks?: string) {
-    super(message);
+  /**
+   * {@inheritDoc app.LogPayload.message}
+   */
+  public readonly remarks?: string;
+  /**
+   * {@inheritDoc app.LogPayload.object}
+   */
+  public readonly object?: any;
+  /**
+   * {@inheritDoc app.LogPayload.durationMs}
+   */
+  public readonly durationMs?: number;
+  /**
+   * {@inheritDoc app.LogPayload.inspect}
+   */
+  public readonly inspect?: string;
+
+  public constructor(message: string, remarks?: string);
+  public constructor(props: DetailedErrorProps);
+  public constructor(props: string | DetailedErrorProps, remarks?: string) {
+    if (typeof props === 'string') {
+      super(props);
+      this.remarks = remarks;
+    } else {
+      super(props.message);
+      this.remarks = props.remarks;
+      this.object = props.object;
+      this.durationMs = props.durationMs;
+      this.inspect = props.inspect;
+    }
   }
 }
