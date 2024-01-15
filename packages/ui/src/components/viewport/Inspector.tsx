229816f367cc2f6b4ba21e7b26ee461775aae3e0
aarthificial <aarthificial@gmail.com>
2024-01-15 18:08:29 +0100

    feat(ui): make inspector toggleable

--- packages/ui/src/components/viewport/Inspector.tsx
+++ packages/ui/src/components/viewport/Inspector.tsx
@@ -1,11 +1,20 @@
+import {useSignal, useSignalEffect} from '@preact/signals';
+import clsx from 'clsx';
 import {FunctionComponent} from 'preact';
-import {useMemo} from 'preact/hooks';
+import {useEffect, useMemo, useRef} from 'preact/hooks';
 import {useApplication, usePanels} from '../../contexts';
+import {useReducedMotion} from '../../hooks';
+import {IconButton} from '../controls';
+import {ChevronRight} from '../icons/ChevronRight';
 import styles from './Viewport.module.scss';
 
 export function Inspector() {
+  const ref = useRef<HTMLDivElement>();
+  const open = useSignal(true);
+  const render = useSignal(true);
   const {inspection} = useApplication();
   const {inspectors} = usePanels();
+  const reducedMotion = useReducedMotion();
   const lookup = useMemo(() => {
     const lookup = new Map<string, FunctionComponent>();
     for (const config of inspectors) {
@@ -15,13 +24,47 @@ export function Inspector() {
     return lookup;
   }, [inspectors]);
 
+  if (reducedMotion) {
+    render.value = open.value;
+  }
+
+  useSignalEffect(() => {
+    if (open.value) {
+      render.value = true;
+    }
+  });
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element || reducedMotion) {
+      return;
+    }
+    const handle = () => {
+      render.value = open.value;
+    };
+    element.addEventListener('transitionend', handle);
+    return () => element.removeEventListener('transitionend', handle);
+  }, [ref.current, reducedMotion]);
+
   const Component = lookup.get(inspection.value.key);
 
   return Component ? (
-    <div className={styles.inspectorOverlay}>
-      <div className={styles.inspector}>
-        <Component />
+    <>
+      <div
+        ref={ref}
+        className={clsx(styles.inspectorOverlay, open.value && styles.open)}
+      >
+        <div className={styles.inspector}>{render.value && <Component />}</div>
       </div>
-    </div>
+      <IconButton
+        className={clsx(styles.inspectorButton, open.value && styles.open)}
+        title="Show inspector"
+        onClick={() => {
+          open.value = !open.value;
+        }}
+      >
+        <ChevronRight />
+      </IconButton>
+    </>
   ) : null;
 }
