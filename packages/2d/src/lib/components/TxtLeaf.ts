8890740d492bab7666a38ec5d402688dc046a43d
Cactus Puppy <puppycactus@gmail.com>
2024-02-28 15:08:44 -0800

    fixup! fix: prevent slanted fonts from drawing pixels extending beyond the CacheBBox

--- packages/2d/src/lib/components/TxtLeaf.ts
+++ packages/2d/src/lib/components/TxtLeaf.ts
@@ -139,7 +139,9 @@ export class TxtLeaf extends Shape {
       -size.height / 2,
       bbox.width,
       bbox.height,
-    ).expand([0, this.fontSize() * 0.5 * this.absoluteScale().x]).expand(lineWidth * miterLimitCoefficient);
+    )
+      .expand([0, this.fontSize() * 0.5])
+      .expand(lineWidth * miterLimitCoefficient);
   }
 
   protected override applyFlex() {
