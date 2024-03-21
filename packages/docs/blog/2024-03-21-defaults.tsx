229a43c47141c80ae0a9a7d57d82ae6478579e43
aarthificial <aarthificial@gmail.com>
2024-03-21 11:19:04 +0100

    docs: add 3.15.0 release

---
+++ packages/docs/blog/2024-03-21-defaults.tsx
@@ -0,0 +1,7 @@
+import {Txt, withDefaults} from '@motion-canvas/2d';
+
+export const MyTxt = withDefaults(Txt, {
+  fill: 'rgba(255, 255, 255, 0.6)',
+  fontFamily: 'JetBrains Mono',
+  fontSize: 28,
+});
