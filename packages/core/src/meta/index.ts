4743b92d10eed301b473434cc5cdde27882607bb
aarthificial <aarthificial@gmail.com>
2024-12-14 21:16:03 +0100

    fix(core): fix incorrect rollup ordering

--- packages/core/src/meta/index.ts
+++ packages/core/src/meta/index.ts
@@ -4,15 +4,17 @@
  * @packageDocumentation
  */
 
+// Custom order makes rollup arrange the ObjectMetaField correctly
+export * from './MetaField';
+export * from './ObjectMetaField';
+
 export * from './BoolMetaField';
 export * from './ColorMetaField';
 export * from './EnumMetaField';
 export * from './ExporterMetaFile';
-export * from './MetaField';
 export * from './MetaFile';
 export * from './MetaOption';
 export * from './NumberMetaField';
-export * from './ObjectMetaField';
 export * from './RangeMetaField';
 export * from './StringMetaField';
 export * from './Vector2MetaField';
